00dfd7dc5d7b506b6154b729c1940d46c970a391
Daniel Tjandra <danieltjandra@gmail.com>
2014-06-19 16:25:35 -0400

    Initial KitKat TMobile config

---
+++ BoardConfig.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Include p4-common
+-include device/samsung/p4-common/BoardConfigCommon.mk
+
+# Use the non-open-source parts, if they're present
+-include vendor/samsung/p4tmo/BoardConfigVendor.mk
+
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 710934528
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 14200340480
+
+# Try to build the kernel
+#TARGET_KERNEL_SOURCE := kernel/samsung/p4
+#TARGET_KERNEL_CONFIG := cyanogenmod_samsung_p4_usa_tmo-jb_defconfig
+#No don't. It's a waste of time and hardly ever changes (I can't think of anything to add)
+# Keep this as a fallback
+TARGET_PREBUILT_KERNEL := device/samsung/p4tmo/KitKatKernel
+#TARGET_PREBUILT_KERNEL := device/samsung/p4tmo/minimal-zImage3
+
